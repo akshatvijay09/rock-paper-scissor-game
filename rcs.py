b57640b2940c62e9fd88a8c5faf57e3e98a9c30b
akshatvijay09 <90043753+akshatvijay09@users.noreply.github.com>
2021-12-22 23:57:50 +0530

    Update rcs.py

--- rcs.py
+++ rcs.py
@@ -1,33 +1,32 @@
 import random
-while(con):
-    def game(comp, plyr):
-        if comp == plyr:
-            print("Tie!!!")
-        elif comp == 'r':
-            if plyr == 'p':
+
+def game(comp, plyr):
+    if comp == plyr:
+        print("Tie!!!")
+    elif comp == 'r':
+        if plyr == 'p':
+            print("You win!!!")
+        elif plyr == 's':
+            print("You Lose!!!")
+    elif comp == 'p':
+            if plyr == 's':
                 print("You win!!!")
-            elif plyr == 's':
+            elif plyr == 'r':
                 print("You Lose!!!")
-        elif comp == 'p':
-                if plyr == 's':
-                    print("You win!!!")
-                elif plyr == 'r':
-                    print("You Lose!!!")
-        elif comp == 's':
-                if plyr == 'r':
-                    print("You win!!!")
-                elif plyr == 'p':
-                    print("You Lose!!!")
+    elif comp == 's':
+            if plyr == 'r':
+                print("You win!!!")
+            elif plyr == 'p':
+                print("You Lose!!!")
 
-    rand = random.randint(1,3)
-    if rand == 1:
-        comp = 'r'
-    elif rand == 2:
-        comp = 'p'
-    else:
-        comp = 's'
-
-    plyr = input("Your Turn: ")
-    print("comp choose ", comp)
-    print("You choose ", plyr)
-    game(comp,plyr)
+rand = random.randint(1,3)
+if rand == 1:
+    comp = 'r'
+elif rand == 2:
+    comp = 'p'
+else:
+    comp = 's'
+plyr = input("Your Turn: ")
+print("comp choose ", comp)
+print("You choose ", plyr)
+game(comp,plyr)
