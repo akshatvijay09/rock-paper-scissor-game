03a988db2762316de142db2884e5ee0ae118da35
akshatvijay09 <90043753+akshatvijay09@users.noreply.github.com>
2021-12-22 22:13:21 +0530

    Add files via upload

---
+++ rcs.py
@@ -0,0 +1,34 @@
+import random
+con = 1
+while(con):
+    def game(comp, plyr):
+        if comp == plyr:
+            print("Tie!!!")
+        elif comp == 'r':
+            if plyr == 'p':
+                print("You win!!!")
+            elif plyr == 's':
+                print("You Lose!!!")
+        elif comp == 'p':
+                if plyr == 's':
+                    print("You win!!!")
+                elif plyr == 'r':
+                    print("You Lose!!!")
+        elif comp == 's':
+                if plyr == 'r':
+                    print("You win!!!")
+                elif plyr == 'p':
+                    print("You Lose!!!")
+
+    rand = random.randint(1,3)
+    if rand == 1:
+        comp = 'r'
+    elif rand == 2:
+        comp = 'p'
+    else:
+        comp = 's'
+
+    plyr = input("Your Turn: ")
+    print("comp choose ", comp)
+    print("You choose ", plyr)
+    game(comp,plyr)
